eb5585c5cdf6539a692324cff7056b2234bedbc8
Van <lvanlowe@inrvisions.com>
2020-06-17 08:46:26 -0400

    updated drawers for mwnu layout

--- xroads-app/src/app/app.component.ts
+++ xroads-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DrawerItem, DrawerSelectEvent } from '@progress/kendo-angular-layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Crossroads PCA Web App';
+  public selected = 'Deacons';
+
+  public items: Array<DrawerItem> = [
+      { text: 'Deacons', icon: 'k-i-wrench', selected: true },
+      { separator: true },
+      { text: 'Nursery', icon: 'k-i-preview' },
+      { text: 'Ushers', icon: 'k-i-gears' },
+      // { separator: true },
+      // { text: 'Attachments', icon: 'k-i-hyperlink-email' },
+      // { text: 'Favourites', icon: 'k-i-star-outline' }
+  ];
+
+  public onSelect(ev: DrawerSelectEvent): void {
+      this.selected = ev.item.text;
+  }
 }
