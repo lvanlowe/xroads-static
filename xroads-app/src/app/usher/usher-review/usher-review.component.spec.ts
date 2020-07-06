6eabe3d7b82e4d10576edc5fdc7078c4bd27dd1a
Van <lvanlowe@inrvisions.com>
2020-07-06 09:31:42 -0400

    add roles to usher

--- xroads-app/src/app/usher/usher-review/usher-review.component.spec.ts
+++ xroads-app/src/app/usher/usher-review/usher-review.component.spec.ts
@@ -4,22 +4,42 @@ import { UsherReviewComponent } from './usher-review.component';
 
 describe('UsherReviewComponent', () => {
   let component: UsherReviewComponent;
-  let fixture: ComponentFixture<UsherReviewComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UsherReviewComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UsherReviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new UsherReviewComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When checking roles', () => {
+    it('when user not login should not be able to update', () => {
+      expect(component.canUpdate).toBeFalsy();
+    });
+
+    it('when user login and has admin role should be able to update', () => {
+        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['admin', 'anonymous', 'authenticated']}
+        component.checkRoles();
+        expect(component.canUpdate).toBeTruthy();
+      });
+
+    it('when user login and has usher role should be able to update', () => {
+        component.userInfo = {
+          identityProvider: '',
+          userDetails: '',
+          userId: '',
+          userRoles: ['usher', 'deacon', 'anonymous', 'authenticated']};
+        component.checkRoles();
+        expect(component.canUpdate).toBeTruthy();
+      });
+
+    it('when user login and not usher or admin role should not be able to update', () => {
+        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['nursery', 'anonymous', 'authenticated']};
+        component.checkRoles();
+        expect(component.canUpdate).toBeFalsy();
+      });
+
   });
 });
