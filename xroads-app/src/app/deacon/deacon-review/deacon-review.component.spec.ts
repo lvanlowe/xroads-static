fb5f26083fe46be8076e00fc11ec5c83f31f0810
Van <lvanlowe@inrvisions.com>
2020-07-06 09:20:37 -0400

    add roles for deacon

--- xroads-app/src/app/deacon/deacon-review/deacon-review.component.spec.ts
+++ xroads-app/src/app/deacon/deacon-review/deacon-review.component.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DeaconReviewComponent } from './deacon-review.component';
 
 describe('DeaconReviewComponent', () => {
   let component: DeaconReviewComponent;
-  let fixture: ComponentFixture<DeaconReviewComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DeaconReviewComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DeaconReviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new DeaconReviewComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When checking roles', () => {
+    it('when user not login should not be able to update', () => {
+      expect(component.canUpdate).toBeFalsy();
+    });
+
+    it('when user login and has admin role should be able to update', () => {
+        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['admin', 'anonymous', 'authenticated']}
+        component.checkRoles();
+        expect(component.canUpdate).toBeTruthy();
+      });
+
+    it('when user login and has deacon role should be able to update', () => {
+        component.userInfo = {
+          identityProvider: '',
+          userDetails: '',
+          userId: '',
+          userRoles: ['usher', 'deacon', 'anonymous', 'authenticated']};
+        component.checkRoles();
+        expect(component.canUpdate).toBeTruthy();
+      });
+
+    it('when user login and not deacon or admin role should not be able to update', () => {
+        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['usher', 'anonymous', 'authenticated']};
+        component.checkRoles();
+        expect(component.canUpdate).toBeFalsy();
+      });
+
   });
 });
--- xroads-app/src/app/deacon/deacon-review/deacon-review.component.ts
+++ xroads-app/src/app/deacon/deacon-review/deacon-review.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from 'src/app/models/user-info';
 
 @Component({
   selector: 'app-deacon-review',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeaconReviewComponent implements OnInit {
 
-  constructor() { }
+  userInfo: UserInfo;
+  canUpdate: boolean;
 
-  ngOnInit() {
+  constructor() {}
+
+
+  async  ngOnInit() {
+
+    this.userInfo = await this.getUserInfo();
+    this.checkRoles();
   }
 
+  checkRoles() {
+    if (this.userInfo.userRoles.indexOf('admin') > -1) {
+      this.canUpdate = true;
+    }
+    if (this.userInfo.userRoles.indexOf('deacon') > -1) {
+      this.canUpdate = true;
+    }
+
+  }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 }
