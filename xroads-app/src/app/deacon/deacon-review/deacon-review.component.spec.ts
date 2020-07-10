1720338bbf67b0cdc8bf4f4102160ca53540f504
Van <lvanlowe@inrvisions.com>
2020-07-10 10:50:26 -0400

    fixed unit test

--- xroads-app/src/app/deacon/deacon-review/deacon-review.component.spec.ts
+++ xroads-app/src/app/deacon/deacon-review/deacon-review.component.spec.ts
@@ -2,41 +2,16 @@ import { DeaconReviewComponent } from './deacon-review.component';
 
 describe('DeaconReviewComponent', () => {
   let component: DeaconReviewComponent;
+  let mockStore;
 
   beforeEach(() => {
-    component = new DeaconReviewComponent();
+
+    mockStore = jasmine.createSpyObj('store', ['dispatch']);
+    component = new DeaconReviewComponent(mockStore);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('When checking roles', () => {
-    it('when user not login should not be able to update', () => {
-      expect(component.canUpdate).toBeFalsy();
-    });
-
-    it('when user login and has admin role should be able to update', () => {
-        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['admin', 'anonymous', 'authenticated']}
-        component.checkRoles();
-        expect(component.canUpdate).toBeTruthy();
-      });
-
-    it('when user login and has deacon role should be able to update', () => {
-        component.userInfo = {
-          identityProvider: '',
-          userDetails: '',
-          userId: '',
-          userRoles: ['usher', 'deacon', 'anonymous', 'authenticated']};
-        component.checkRoles();
-        expect(component.canUpdate).toBeTruthy();
-      });
-
-    it('when user login and not deacon or admin role should not be able to update', () => {
-        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['usher', 'anonymous', 'authenticated']};
-        component.checkRoles();
-        expect(component.canUpdate).toBeFalsy();
-      });
-
-  });
 });
--- xroads-app/src/app/usher/usher-review/usher-review.component.spec.ts
+++ xroads-app/src/app/usher/usher-review/usher-review.component.spec.ts
@@ -2,42 +2,16 @@ import { UsherReviewComponent } from './usher-review.component';
 
 describe('UsherReviewComponent', () => {
   let component: UsherReviewComponent;
-
+  let mockStore;
 
   beforeEach(() => {
-    component = new UsherReviewComponent();
+
+    mockStore = jasmine.createSpyObj('store', ['dispatch']);
+    component = new UsherReviewComponent(mockStore);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('When checking roles', () => {
-    it('when user not login should not be able to update', () => {
-      expect(component.canUpdate).toBeFalsy();
-    });
-
-    it('when user login and has admin role should be able to update', () => {
-        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['admin', 'anonymous', 'authenticated']}
-        component.checkRoles();
-        expect(component.canUpdate).toBeTruthy();
-      });
-
-    it('when user login and has usher role should be able to update', () => {
-        component.userInfo = {
-          identityProvider: '',
-          userDetails: '',
-          userId: '',
-          userRoles: ['usher', 'deacon', 'anonymous', 'authenticated']};
-        component.checkRoles();
-        expect(component.canUpdate).toBeTruthy();
-      });
-
-    it('when user login and not usher or admin role should not be able to update', () => {
-        component.userInfo = {identityProvider: '', userDetails: '', userId: '', userRoles: ['nursery', 'anonymous', 'authenticated']};
-        component.checkRoles();
-        expect(component.canUpdate).toBeFalsy();
-      });
-
-  });
 });
