11a9a16c5d866e4d71dfa4ca49acfd7536cbc0dd
Van <lvanlowe@inrvisions.com>
2020-12-18 08:44:53 -0500

    add addattendee function

---
+++ api/api/Models/AttendeeDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace api.Models
+{
+    public class AttendeeDB
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string cell { get; set; }
+        public string email { get; set; }
+        public bool isDeacon { get; set; }
+
+    }
+}
