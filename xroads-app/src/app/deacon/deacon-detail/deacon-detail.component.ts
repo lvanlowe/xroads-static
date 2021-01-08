550416b8d683f3378a971e819a17055a7aa985c3
Van <lvanlowe@inrvisions.com>
2021-01-08 10:31:11 -0500

    update deacon detail screen

--- xroads-app/src/app/deacon/deacon-detail/deacon-detail.component.ts
+++ xroads-app/src/app/deacon/deacon-detail/deacon-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-deacon-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deacon-detail.component.scss']
 })
 export class DeaconDetailComponent implements OnInit {
+
+  deaconForm: FormGroup;
 
   constructor() { }
 
